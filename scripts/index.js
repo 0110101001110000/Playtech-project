b2f4f29f5773706e58537abcc8fea9cc75861df2
01101010-01110000 <0110101001110000_WK@proton.me>
2024-05-13 12:13:39 -0300

    Create 'smooth scroll to center' funcionality

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,54 @@
 /* Init -------------------------------------------------------------------- */
 
 
+/* Smooth Scroll Functionality ----- */
+
+// Get elements
+const portifolios_ancor = document.getElementById("portifolios-ancor");
+const prices_ancor = document.getElementById("plans-ancor");
+const about_ancor = document.getElementById("about-ancor");
+const portifolios_section = document.getElementById("portifolios");
+const prices_section = document.getElementById("prices");
+const about_section = document.getElementById("about");
+
+// Scroll Function
+function scrollToElementCenter(button, element) {
+  button.addEventListener("click", (event) => {
+    
+    // Get position and sizes values
+    const window_height = parseInt(window.innerHeight);
+    const element_rect = element.getBoundingClientRect();
+    const element_y = parseInt(element_rect.y);
+    const element_height = parseInt(element_rect.height);
+    
+    // Set coordinate "y" to scroll
+    const coord_y = element_y - ((window_height - element_height) / 2);  
+
+    // Scroll to coordinate
+    if (window_height > element_height) {
+      window.scroll({
+        top: coord_y,
+        behavior: "smooth"
+      });
+    } else {
+      window.scroll({
+        top: (element_y - 10),
+        behavior: "smooth"
+      });
+    }  
+  });
+}
+
+// Smooth Scroll to Portifolios Section
+scrollToElementCenter(portifolios_ancor, portifolios_section);
+
+// Smooth Scroll to Prices Section
+scrollToElementCenter(prices_ancor, prices_section);
+
+// Smooth Scroll to Prices Section
+scrollToElementCenter(about_ancor, about_section);
+
+
 /* Slider Functionality ------------ */
 
 // Get elements
